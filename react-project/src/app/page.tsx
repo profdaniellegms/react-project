9523ed986249582d527220f96abfafa838e8a71d
profdaniellegms <profdaniellealmeidagms@gmail.com>
2024-09-23 21:32:36 -0300

    map

--- react-project/src/app/page.tsx
+++ react-project/src/app/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import { FiTrash, FiEdit, FiCheck } from "react-icons/fi"
+import { api } from "./api";
+
+interface TaskProps {
+  id: string;
+  description: string;
+  date: string;
+  status: boolean;
+}
+
+// Inicializa lista de tarefas como lista vazia
+const [tasks, setTasks] = useState<TaskProps[]>([])
+
+// Ao renderizar a página, chama a função "loadTasks"
+useEffect(() => {
+  loadTasks();
+}, [])
+
+// Busca as tarefas no banco de dados via API
+async function loadTasks() {
+  const response = await api.get("/tasks")
+  setTasks(response.data)
+}
 
 export default function Home() {
   return (
@@ -16,14 +38,17 @@ export default function Home() {
           </form>
         </section>
         <section className="mt-5 flex flex-col">
+
+          {tasks.map( (task) => (
             <article className="w-full bg-slate-200 text-slate-800 p-2 rounded relative hover:bg-sky-300">
-              <p>Task 1</p>
-              <p>01/01/2025</p>
-              <p>Done</p>
-              <button className="flex absolute right-14 -top-2 bg-green-600 w-7 h-7 items-center justify-center text-slate-200"><FiCheck></FiCheck></button>
-              <button className="flex absolute right-7 -top-2 bg-yellow-500 w-7 h-7 items-center justify-center text-slate-200"><FiEdit></FiEdit></button>
-              <button className="flex absolute right-0 -top-2 bg-red-600 w-7 h-7 items-center justify-center text-slate-200"><FiTrash></FiTrash></button>
+                <p>{task.description}</p>
+                <p>{task.date}</p>
+                <p>{task.status}</p>
+                <button className="flex absolute right-14 -top-2 bg-green-600 w-7 h-7 items-center justify-center text-slate-200"><FiCheck></FiCheck></button>
+                <button className="flex absolute right-7 -top-2 bg-yellow-500 w-7 h-7 items-center justify-center text-slate-200"><FiEdit></FiEdit></button>
+                <button className="flex absolute right-0 -top-2 bg-red-600 w-7 h-7 items-center justify-center text-slate-200"><FiTrash></FiTrash></button>
             </article>
+          ))}
         </section>
       </main>
     </div>
